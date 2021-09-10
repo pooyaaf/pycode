6457dc7f07850e03adc83a5956a3ee7b43a5abb2
pooyaaf <pooya1380@gmail.com>
2021-09-10 17:54:13 +0430

    built-in

--- test1.py
+++ test1.py
@@ -2,4 +2,8 @@ numbers=[int(number) for number in input("enter numbers: ").split()]
 sum=print("sum is :",sum(numbers))
 maximum=print("max is :",max(numbers))
 minimum=print("min is :",min(numbers))
-#commit1
+#sort
+numbers.sort()
+print("sorted:",numbers)
+numbers.reverse()
+print("reverse:",numbers)
