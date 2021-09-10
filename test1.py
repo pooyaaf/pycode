b46ea6acccd4a9de4fd4d65e1bf727c86808cc05
pooyaaf <pooya1380@gmail.com>
2021-09-10 17:45:48 +0430

    push

--- test1.py
+++ test1.py
@@ -2,4 +2,4 @@ numbers=[int(number) for number in input("enter numbers: ").split()]
 sum=print("sum is :",sum(numbers))
 maximum=print("max is :",max(numbers))
 minimum=print("min is :",min(numbers))
-#commit
+#commit1
