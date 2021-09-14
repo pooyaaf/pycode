1ac27df22841950e3acabc0a20cd6bdaf0057437
pooyaaf <pooya1380@gmail.com>
2021-09-14 10:06:24 +0430

    data_manipulating

--- webScrap/digiTest.py
+++ webScrap/digiTest.py
@@ -1,6 +1,11 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-for i in range(30):
+import pandas as pd 
+#pd:
+data = []
+removal_words = ['sim','dual' , 'single' , 'mobile phone']
+#
+for i in range(3):
     with urlopen(f"https://www.digikala.com/search/category-mobile-phone/?has_selling_stock=1&pageno={i+1}") as response:
         html=response.read().decode("utf-8") #1- request to url
         soup=BeautifulSoup(html,"html.parser") #2 - parse html
@@ -10,10 +15,20 @@ for i in range(30):
                 name=mobile.find("div",{"class":"c-product-box__title-en"}).text
             except AttributeError:
                 name="not found"
-            print(f"{i+1} - {name}",end=" - ")
+         
             try:
                 price=mobile.find("div",{"class":"c-price__value-wrapper"}).text.strip().split("\n")[0]
             except AttributeError:
                 price="price not found!"
-            print(price)
+           # print(name,price)
+            #CSV: Comma Seperated Values
+            name=name.lower()
+            sim = 2 if name.count('dual-sim') != 0 else 1
+            for word in removal_words:
+                name = name.replace(word,'')
+            words = name.split()
+            name=' '.join(words[1:])
+            data.append([name,words[0],sim,int(price.replace(',',''))])
 
+df = pd.DataFrame(data,columns=["name","brand","No.Sim","price"])
+df.to_csv("mobiles.csv",index=False)
